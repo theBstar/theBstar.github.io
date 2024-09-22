925f8257557b592136887a59f21ba1da33e3bf47
bikram <6bikramsutar@gmail.com>
2024-09-22 15:17:14 +0530

    added homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+import { Github, Linkedin, Mail, MapPin, Twitter } from 'lucide-react'
+import Image from 'next/image'
 
-export default function Home() {
+export default function Portfolio() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-gradient-to-r from-gray-900 to-gray-300 p-8 flex items-center">
+      <div className="max-w-[1080px] mx-auto bg-white/1 backdrop-blur-md rounded-lg shadow-lg overflow-hidden">
+        <div className="flex flex-col md:flex-row min-h-[600px]">
+          {/* Left column */}
+          <div className="md:w-1/3 p-8 text-white">
+            <div className="mb-6">
+              <Image
+                src="/bikram_pp.png"
+                alt="Profile"
+                width={200}
+                height={200}
+                style={{ width: 200, height: 200 }}
+                className="rounded-full mx-auto object-cover filter grayscale"
+              />
+            </div>
+            <h1 className="text-4xl font-bold mb-2">Bikram Sutar</h1>
+            <p className="text-gray-400 text-xs mb-4 flex items-center gap-1"><MapPin size={".75rem"} /> Tezpur, Assam</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <h2 className="text-lg text-gray-300 mb-1">Founding Engineer @ Houseware</h2>
+            <p className="text-gray-400 mb-6">Prev @ OrangeHealth</p>
+
+            <div className="flex justify-left space-x-8 mb-4  ">
+              <SocialLink href="https://x.com/thebstar13" icon={<Twitter className="w-5 h-5" />} label="Twitter" />
+              <SocialLink href="https://github.com/thebstar" icon={<Github className="w-5 h-5" />} label="GitHub" />
+              <SocialLink href="https://www.linkedin.com/in/thebstar/" icon={<Linkedin className="w-5 h-5" />} label="LinkedIn" />
+              <SocialLink href="mailto:6bikramsutar@gmail.com" icon={<Mail className="w-5 h-5" />} label="Email" />
+            </div>
+
+            <a
+              href={"https://www.thebstar.com/blogs/"}
+              className="text-gray-300 hover:text-white transition-colors duration-200 underline"
+              aria-label={"Read blogs"}
+            >
+              Read blogs
+            </a>
+          </div>
+
+          {/* Right column */}
+          <div className="md:w-2/3 p-8 bg-white/30 flex flex-col justify-center">
+            <h2 className="text-4xl font-bold mb-4 text-gray-700">Hi 👋</h2>
+            <p className="text-lg text-gray-800">
+              I am glad you are here. I am a hustler and a builder who loves to make an impact in people&#39;s lives. I have been building software since 2018. Building software to serve thousands of users for their diagnostic needs during the COVID-19 pandemic was the most satisfying work I have done.
+              Now @Houseware, being the first hire, I have seen company building from scratch. I have been instrumental in building the core product and the culture.
+            </p>
+
+            <p className="text-lg text-gray-700 mt-2">
+              When not building, I play a bit of Cricket, I run a community Cricket club with more than 50 members. I also do a bit of running. 1:52 is my latest half marathon PB (April 2023).
+            </p>
+            <p className="text-lg text-gray-700 mt-2">
+              Since July 2024, I have been running community events for kids in my area with a friend. Collectively more than 100 kids have participated.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
-  );
+  )
+}
+
+type SocialLinkProps = {
+  href: string
+  icon: JSX.Element
+  label: string
+}
+
+function SocialLink({ href, icon, label }: SocialLinkProps) {
+  return (
+    <a
+      href={href}
+      className="text-gray-300 hover:text-white transition-colors duration-200"
+      aria-label={label}
+      target='_blank'
+    >
+      {icon}
+    </a>
+  )
 }
