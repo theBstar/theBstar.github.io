3b7f5f5677fef418a6fa43e2534792da1e2b3751
bikram <6bikramsutar@gmail.com>
2024-09-22 15:32:36 +0530

    change next config to js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
